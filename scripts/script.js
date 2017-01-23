0178a3ae7578b5a21477807b0587c493df275eb2
funkeyy <funkythrowaway@gmail.com>
2017-01-23 08:05:36 +0000

    FIX
    FIX

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -16,8 +15,7 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-
-CountDownTimer('01/19/2017 11:59 PM', 'countdown');
+CountDownTimer('01/26/2017 11:59 PM', 'countdown');
 
 function CountDownTimer(dt, id) {
     var end = new Date(dt);
@@ -47,53 +45,3 @@ function CountDownTimer(dt, id) {
 
     timer = setInterval(showRemaining, 1000);
 }
-=======
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    h = checkTime(h);
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('timeDisplay').innerHTML = "<h1>" +
-    h + ":" + m + "</h1>";
-    var t = setTimeout(startTime, 500);
-}
-
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
-CountDownTimer('01/19/2017 11:59 PM', 'countdown');
-
-function CountDownTimer(dt, id) {
-    var end = new Date(dt);
-    var _second = 1000;
-    var _minute = _second * 60;
-    var _hour = _minute * 60;
-    var _day = _hour * 24;
-    var timer;
-
-    function showRemaining() {
-        var now = new Date();
-        var distance = end - now;
-        if (distance < 0) {
-
-            clearInterval(timer);
-            document.getElementById(id).innerHTML = 'EXPIRED!';
-
-            return;
-        }
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
-
-        document.getElementById(id).innerHTML = '<div class="timeHolder">' + days + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + hours + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + minutes + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + seconds + '</div>';
-    }
-
-    timer = setInterval(showRemaining, 1000);
-}
->>>>>>> origin/master
