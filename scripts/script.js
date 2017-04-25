df4f6ec86c3c4a66b52f75427378fb3a80de4ff4
funkeyy <funkythrowaway@gmail.com>
2017-04-25 03:04:32 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-CountDownTimer('03/27/2017 11:59 PM', 'countdown');
+CountDownTimer('04/27/2017 11:59 PM', 'countdown');
 
 function CountDownTimer(dt, id) {
     var end = new Date(dt);
